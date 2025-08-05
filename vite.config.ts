b09c6d4cd81551ea31c05659e23e1c25dbc01c45
roubegpt <roubegpt@gmail.com>
2025-08-06 05:10:01 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      "@": "/src",
-      "@shared": "/shared",
-      "@assets": "/attached_assets"
-    }
-  }
+  root: "client",
+  build: {
+    outDir: "../dist"
+  },
+  plugins: [react()]
 });
