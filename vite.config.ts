16f382a1671ee7fc964d63ada14c5bd9bc317fc0
roubegpt <roubegpt@gmail.com>
2025-08-06 05:27:01 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/roubegpt-MangoFlow--4-/', // 깃허브 저장소 이름과 맞게 설정
+  base: '/roubegpt-MangoFlow--4-/', // 깃허브 페이지 경로에 맞게
+  root: 'client',  // root 디렉토리를 client로 지정
   plugins: [react()],
   server: {
     port: 5173,
