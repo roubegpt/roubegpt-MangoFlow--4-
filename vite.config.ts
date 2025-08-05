c905e2426c19cf492e690e5858a75322ba2f0670
roubegpt <roubegpt@gmail.com>
2025-08-06 06:49:17 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// 배포 경로(base) 설정 필요
 export default defineConfig({
+  root: "client",
   base: "/roubegpt-MangoFlow--4-/",
-  plugins: [react(), tsconfigPaths()],
   build: {
     outDir: "../dist"
-  }
+  },
+  plugins: [react(), tsconfigPaths()]
 });
