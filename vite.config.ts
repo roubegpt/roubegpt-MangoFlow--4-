d0e474bae84023256fc8ef7baf074b3778aa57d0
roubegpt <roubegpt@gmail.com>
2025-08-07 07:36:11 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,3 +10,15 @@ export default defineConfig({
     emptyOutDir: true,
   },
 });
+mport { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  root: "client",
+  base: "/roubegpt-MangoFlow--4-/",
+  plugins: [react()],
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
+});
