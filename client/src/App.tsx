27a0f4ace68a3c43ed2166ed17d9d82eefad70fc
roubegpt <roubegpt@gmail.com>
2025-08-07 07:10:40 +0900

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function App() {
-  return <div>Hello MangoFlow</div>;
+  return (
+    <div className="text-center p-8">
+      <h1 className="text-4xl font-bold mb-4">MangoFlow</h1>
+      <p className="text-lg">Welcome to the MangoFlow app!</p>
+    </div>
+  );
 }
 
 export default App;
