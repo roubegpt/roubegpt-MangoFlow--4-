108c6c4dde9ab6f90c72992d8594f8d06568657c
roubegpt <roubegpt@gmail.com>
2025-08-07 00:04:46 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/roubegpt-MangoFlow--4-/",
   root: "client",
+  base: "/roubegpt-MangoFlow--4-/",
   plugins: [react()]
 });
