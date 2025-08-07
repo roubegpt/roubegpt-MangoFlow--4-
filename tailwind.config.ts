c6e53f83c7bba2b6401d1be6de18fbbe58eb3031
roubegpt <roubegpt@gmail.com>
2025-08-07 17:23:26 +0900

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,9 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./client/src/**/*.{ts,tsx,js,jsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./index.html"],
   theme: {
-    extend: {
-      borderColor: {
-        border: "hsl(20, 5.9%, 90%)", // --border 커스텀 컬러 정의
-      },
-      colors: {
-        primary: "hsl(200, 95%, 55%)", // 필요에 따라 추가 가능
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
